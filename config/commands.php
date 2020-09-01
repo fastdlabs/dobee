e5d809c39acac0dab4d86d663622115a1223b265
janhuang <bboyjanhuang@gmail.com>
2020-09-01 15:59:43 +0800

    添加默认process

---
+++ config/commands.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    \Console\DemoConsole::class,
+];
---
+++ src/Process/DemoProcess.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Process;
+
+
+use FastD\Swoole\Process\Process;
+
+class DemoProcess extends Process
+{
+    public function handle(): void
+    {
+        echo 'run process';
+    }
+
+    public function exit(int $pid, int $code, int $signal): void
+    {
+        print_r(func_get_args());
+    }
+}
